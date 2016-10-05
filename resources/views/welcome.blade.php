a009ccab0d418a36aa4d5e5adfc7d8ea95e5dda0
Jesús Arriaga <jarriagabarron@gmail.com>
2016-10-05 16:20:53 -0500

    body cover background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ChécaloAquí.com</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,500" rel="stylesheet" type="text/css">
@@ -13,7 +13,6 @@
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <link href="/css/styles.css" rel="stylesheet" type="text/css">
 
 
 
@@ -28,8 +27,15 @@
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
-                height: 100vh;
                 margin: 0;
+                /*css for full size background image*/
+                background: url('/images/background-street.jpg') no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+
+                height: 100%;
             }
 
             .full-height {
@@ -53,14 +59,23 @@
                 z-index: 99;
             }
 
+            .top-left {
+                position: absolute;
+                left: 20px;
+                top: 18px;
+                z-index: 99;
+            }
+
+
             .content {
                 z-index: 99;
                 text-align: center;
             }
 
             .title {
-                font-size: 84px;
+                font-size: 64px;
                 color: #fff;
+                margin-bottom: 50px;
             }
 
             .links > a {
@@ -73,49 +88,33 @@
                 text-transform: uppercase;
             }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-
-            @media all and (max-width:768px){
-                .container{
-                    min-width: 320px;
-                }
-                .title{
-                    font-size: 42px;
-                }
-                .btn-landing{
-                    min-height: 80px;
-                    margin-bottom: 20px;
-                    font-size: 11px;
-                    font-weight: 600;
-                }
-                .btn-landing h2{
-                    font-size: 20px;
-                    margin: 5px;
-                    font-weight: 600;
-                }
-
-            }
         </style>
+        <link href="/css/styles.css" rel="stylesheet" type="text/css">
+
     </head>
     <body >
         <div class="overlay"></div>
-        <div class="flex-center position-ref full-height bg-img ">
+        <div class="container ">
             @if (Route::has('login'))
                 <div class="top-right links">
                     <a href="{{ url('/login') }}">{{ trans('app.Login') }}</a>
                     <a href="{{ url('/register') }}"> {{ trans('app.Register') }}</a>
                 </div>
+                <div class="top-left">
+                    <img src="images/fondo-b-250x250.png" style="width:120px;height: auto">
+                </div>
             @endif
 
-            <div class="content container">
-                <div class="title m-b-md">
-                    Chécalo Aquí
+            <div class="wrapper-content">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                        <div class="title m-b-md text-center">
+                            Buscas el mejor precio?
+                        </div>
+                    </div>
                 </div>
-                    <div class="row">
-                        <div class="col-md-5 col-sm-6">
+                <div class="row">
+                        <div class="col-md-4 col-md-offset-1 col-sm-6 text-center">
                             <a class="btn btn-lg btn-block btn-success btn-landing">
                                 <br>Quieres cazar las mejores ofertas?<br>
                                 <h2><i class="fa fa-shopping-cart" aria-hidden="true"></i> Click aquí</h2>
@@ -124,13 +123,13 @@
                         <div class="col-md-2">
 
                         </div>
-                        <div class="col-md-5 col-sm-6">
+                        <div class="col-md-4 col-sm-6 text-center">
                             <a class="btn btn-lg btn-block btn-warning btn-landing">
                                 Quieres anunciar tu negocio o servicios,<br> y llevarlo al siguiente nivel?
                                 <h2><i class="fa fa-rocket" aria-hidden="true"></i> Click aquí</h2>
                             </a>
                         </div>
-                    </div>
+                </div>
             </div>
 
         </div>
